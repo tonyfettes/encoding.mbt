2cdf8fe2190ab4192c6ce5e4dc1ebf47254cd5d2
Haoxiang Fei <feihaoxiang@idea.edu.cn>
2025-07-19 17:35:15 +0800

    perf(v2): use simple automatic vectorization

--- src/v2/utf8.c
+++ src/v2/utf8.c
@@ -7,193 +7,114 @@ struct moonbit_int32_array {
   int32_t *data;
 };
 
-MOONBIT_FFI_EXPORT
-int32_t
-moonbit_encoding_v2_utf8_decode_0(
-  const unsigned char *restrict s,
-  int32_t soff,
-  int32_t slen,
+struct moonbit_bytes_view {
+  int32_t offset;
+  int32_t length;
+  moonbit_bytes_t bytes;
+};
+
+MOONBIT_FFI_EXPORT int
+tonyfettes_encoding_v2_decode_utf8_to_char_array(
+  struct moonbit_bytes_view s,
   struct moonbit_int32_array *restrict t
 ) {
-  const unsigned char *sptr = s + soff;
-  while (slen > 8) {
-    uint64_t b = ((uint64_t *)sptr)[0];
-    if ((b & 0x8080808080808080) == 0) {
-      t->data[t->size] = sptr[0];
-      t->data[t->size + 1] = sptr[1];
-      t->data[t->size + 2] = sptr[2];
-      t->data[t->size + 3] = sptr[3];
-      t->data[t->size + 4] = sptr[4];
-      t->data[t->size + 5] = sptr[5];
-      t->data[t->size + 6] = sptr[6];
-      t->data[t->size + 7] = sptr[7];
-      t->size += 8;
-      sptr += 8;
-      slen -= 8;
-      continue;
-    } else {
-      break;
-    }
-  }
-  while (slen > 4) {
-    uint32_t b;
+  const unsigned char *sptr = s.bytes + s.offset;
+  int32_t slen = s.length;
+  while (true) {
     unsigned char c0;
     unsigned char c1;
     unsigned char c2;
     unsigned char c3;
-    b = ((uint32_t *)sptr)[0];
-    c0 = sptr[0];
-    c1 = sptr[1];
-    c2 = sptr[2];
-    c3 = sptr[3];
-    if ((b & 0x80808080) == 0) {
-      t->data[t->size] = c0;
-      t->data[t->size + 1] = c1;
-      t->data[t->size + 2] = c2;
-      t->data[t->size + 3] = c3;
-      t->size += 4;
-      sptr += 4;
-      slen -= 4;
-      continue;
-    }
-    if (c0 <= 0x7F) {
-      goto vec_utf8_1_byte;
-    }
-    if (c0 >= 0xC2 && c0 <= 0xDF) {
-      if (c1 >= 0x80 && c1 <= 0xBF) {
-        if (c2 >= 0xC2 && c2 <= 0xDF && c3 >= 0x80 && c3 <= 0xBF) {
-          t->data[t->size] = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
-          t->data[t->size + 1] = ((c2 & 0x1F) << 6) | (c3 & 0x3F);
-          t->size += 2;
-          sptr += 4;
-          slen -= 4;
-          continue;
-        } else {
-          goto vec_utf8_2_bytes;
-        }
-      } else {
-        return -1;
-      }
-    }
-    if (c0 == 0xE0) {
-      if ((c1 >= 0xA0 && c1 <= 0xBF) && (c2 >= 0x80 && c2 <= 0xBF)) {
-        goto vec_utf8_3_bytes;
-      } else {
-        return -1;
-      }
-    }
-    if (c0 == 0xED) {
-      if ((c1 >= 0x80 && c1 <= 0x9F) && (c2 >= 0x80 && c2 <= 0xBF)) {
-        goto vec_utf8_3_bytes;
-      } else {
-        return -1;
-      }
-    }
-    if (c0 >= 0xE1 && c0 <= 0xEF) {
-      if ((c1 >= 0x80 && c1 <= 0xBF) && (c2 >= 0x80 && c2 <= 0xBF)) {
-        goto vec_utf8_3_bytes;
-      } else {
-        return -1;
-      }
-    }
-    if (c0 == 0xF0) {
-      if ((c1 >= 0x90 && c1 <= 0xBF) && (c2 >= 0x80 && c2 <= 0xBF) &&
-          (c3 >= 0x80 && c3 <= 0xBF)) {
-        goto vec_utf8_4_bytes;
-      } else {
-        return -1;
-      }
-    }
-    if (c0 >= 0xF1 && c0 <= 0xF3) {
-      if ((c1 >= 0x80 && c1 <= 0xBF) && (c2 >= 0x80 && c2 <= 0xBF) &&
-          (c3 >= 0x80 && c3 <= 0xBF)) {
-        goto vec_utf8_4_bytes;
-      } else {
-        return -1;
-      }
-    }
-    if (c0 == 0xF4) {
-      if ((c1 >= 0x80 && c1 <= 0x8F) && (c2 >= 0x80 && c2 <= 0xBF) &&
-          (c3 >= 0x80 && c3 <= 0xBF)) {
-        goto vec_utf8_4_bytes;
-      } else {
-        return -1;
-      }
-    }
-    return -1;
-  vec_utf8_1_byte:
-    t->data[t->size] = c0;
-    t->size++;
-    sptr += 1;
-    slen -= 1;
-    continue;
-  vec_utf8_2_bytes:
-    t->data[t->size] = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
-    t->size++;
-    sptr += 2;
-    slen -= 2;
-    continue;
-  vec_utf8_3_bytes:
-    t->data[t->size] = ((c0 & 0x0F) << 12) | ((c1 & 0x3F) << 6) | (c2 & 0x3F);
-    t->size++;
-    sptr += 3;
-    slen -= 3;
-    continue;
-  vec_utf8_4_bytes:
-    t->data[t->size] = ((c0 & 0x07) << 18) | ((c1 & 0x3F) << 12) |
-                       ((c2 & 0x3F) << 6) | (c3 & 0x3F);
-    t->size++;
-    sptr += 4;
-    slen -= 4;
-    continue;
-  }
-  while (slen > 0) {
-    unsigned char c0;
-    unsigned char c1;
-    unsigned char c2;
-    unsigned char c3;
-    c0 = sptr[0];
-    if (c0 <= 0x7F) {
-      goto utf8_1_byte;
-    }
-    if (slen < 2) {
+    if (slen == 0) {
       return -1;
     }
-    c1 = sptr[1];
-    if (c0 >= 0xC2 && c0 <= 0xDF) {
-      if (c1 >= 0x80 && c1 <= 0xBF) {
-        goto utf8_2_bytes;
+    c0 = sptr[0];
+    if (c0 <= 0x7F) {
+      if (slen >= 8) {
+        unsigned char c1 = sptr[1];
+        unsigned char c2 = sptr[2];
+        unsigned char c3 = sptr[3];
+        unsigned char c4 = sptr[4];
+        unsigned char c5 = sptr[5];
+        unsigned char c6 = sptr[6];
+        unsigned char c7 = sptr[7];
+        if (c1 <= 0x7F && c2 <= 0x7F && c3 <= 0x7F && c4 <= 0x7F &&
+            c5 <= 0x7F && c6 <= 0x7F && c7 <= 0x7F) {
+          t->data[t->size] = c0;
+          t->data[t->size + 1] = c1;
+          t->data[t->size + 2] = c2;
+          t->data[t->size + 3] = c3;
+          t->data[t->size + 4] = c4;
+          t->data[t->size + 5] = c5;
+          t->data[t->size + 6] = c6;
+          t->data[t->size + 7] = c7;
+          t->size += 8;
+          sptr += 8;
+          slen -= 8;
+          continue;
+        } else {
+          goto utf8_1_byte;
+        }
       } else {
-        return -1;
+        goto utf8_1_byte;
+      }
+    }
+    if (c0 <= 0xC1) {
+      goto malformed;
+    }
+    if (slen < 2) {
+      goto malformed;
+    }
+    c1 = sptr[1];
+    if (c0 <= 0xDF) {
+      if (c1 >= 0x80 && c1 <= 0xBF) {
+        if (slen >= 4) {
+          c2 = sptr[2];
+          c3 = sptr[3];
+          if (c2 >= 0xC2 && c2 <= 0xDF && c3 >= 0x80 && c3 <= 0xBF) {
+            t->data[t->size] = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
+            t->data[t->size + 1] = ((c2 & 0x1F) << 6) | (c3 & 0x3F);
+            t->size += 2;
+            sptr += 4;
+            slen -= 4;
+            continue;
+          } else {
+            goto utf8_2_bytes;
+          }
+        } else {
+          goto utf8_2_bytes;
+        }
+      } else {
+        goto malformed;
       }
     }
     if (slen < 3) {
-      return -1;
+      goto malformed;
     }
     c2 = sptr[2];
     if (c0 == 0xE0) {
       if ((c1 >= 0xA0 && c1 <= 0xBF) && (c2 >= 0x80 && c2 <= 0xBF)) {
         goto utf8_3_bytes;
       } else {
-        return -1;
+        goto malformed;
       }
     }
     if (c0 == 0xED) {
       if ((c1 >= 0x80 && c1 <= 0x9F) && (c2 >= 0x80 && c2 <= 0xBF)) {
         goto utf8_3_bytes;
       } else {
-        return -1;
+        goto malformed;
       }
     }
-    if (c0 >= 0xE1 && c0 <= 0xEF) {
+    if (c0 <= 0xEF) {
       if ((c1 >= 0x80 && c1 <= 0xBF) && (c2 >= 0x80 && c2 <= 0xBF)) {
         goto utf8_3_bytes;
       } else {
-        return -1;
+        goto malformed;
       }
     }
     if (slen < 4) {
-      return -1;
+      goto malformed;
     }
     c3 = sptr[3];
     if (c0 == 0xF0) {
@@ -201,15 +122,15 @@ moonbit_encoding_v2_utf8_decode_0(
           (c3 >= 0x80 && c3 <= 0xBF)) {
         goto utf8_4_bytes;
       } else {
-        return -1;
+        goto malformed;
       }
     }
-    if (c0 >= 0xF1 && c0 <= 0xF3) {
+    if (c0 <= 0xF3) {
       if ((c1 >= 0x80 && c1 <= 0xBF) && (c2 >= 0x80 && c2 <= 0xBF) &&
           (c3 >= 0x80 && c3 <= 0xBF)) {
         goto utf8_4_bytes;
       } else {
-        return -1;
+        goto malformed;
       }
     }
     if (c0 == 0xF4) {
@@ -217,10 +138,10 @@ moonbit_encoding_v2_utf8_decode_0(
           (c3 >= 0x80 && c3 <= 0xBF)) {
         goto utf8_4_bytes;
       } else {
-        return -1;
+        goto malformed;
       }
     }
-    return -1;
+    goto malformed;
   utf8_1_byte:
     t->data[t->size] = c0;
     t->size++;
@@ -246,6 +167,7 @@ moonbit_encoding_v2_utf8_decode_0(
     sptr += 4;
     slen -= 4;
     continue;
+  malformed:
+    return sptr - s.bytes + s.offset;
   }
-  return 0;
 }
