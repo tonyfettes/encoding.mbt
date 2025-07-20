5c49b9174f48e9a4cc4cd50614f1624bf418e2f8
Tony Fettes <tonyfettes@tonyfettes.com>
2025-07-20 21:29:58 +0800

    perf(v2): use tlen

--- src/v2/utf8.c
+++ src/v2/utf8.c
@@ -68,12 +68,15 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
   unsigned char *sptr = s.bytes + s.offset;
   int32_t slen = s.length;
   moonbit_int32_array_reserve(t, slen);
+  int32_t *restrict tptr = t->data + t->size;
+  int32_t tlen = 0;
   while (true) {
     unsigned char c0;
     unsigned char c1;
     unsigned char c2;
     unsigned char c3;
     if (slen == 0) {
+      t->size += tlen;
       return -1;
     }
     c0 = sptr[0];
@@ -90,11 +93,11 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
           __m512i q1 = _mm512_cvtepu8_epi32(b1);
           __m512i q2 = _mm512_cvtepu8_epi32(b2);
           __m512i q3 = _mm512_cvtepu8_epi32(b3);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size], q0);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size + 16], q1);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size + 32], q2);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size + 48], q3);
-          t->size += 64;
+          _mm512_storeu_si512((__m512i *)&tptr[tlen], q0);
+          _mm512_storeu_si512((__m512i *)&tptr[tlen + 16], q1);
+          _mm512_storeu_si512((__m512i *)&tptr[tlen + 32], q2);
+          _mm512_storeu_si512((__m512i *)&tptr[tlen + 48], q3);
+          tlen += 64;
           sptr += 64;
           slen -= 64;
           continue;
@@ -103,17 +106,17 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
           __m128i b1 = _mm512_extracti32x4_epi32(data, 1);
           __m512i q0 = _mm512_cvtepu8_epi32(b0);
           __m512i q1 = _mm512_cvtepu8_epi32(b1);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size], q0);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size + 16], q1);
-          t->size += 32;
+          _mm512_storeu_si512((__m512i *)&tptr[tlen], q0);
+          _mm512_storeu_si512((__m512i *)&tptr[tlen + 16], q1);
+          tlen += 32;
           sptr += 32;
           slen -= 32;
           continue;
         } else if ((mask & 0xFFFF) == 0) {
           __m128i b0 = _mm512_extracti32x4_epi32(data, 0);
           __m512i q0 = _mm512_cvtepu8_epi32(b0);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size], q0);
-          t->size += 16;
+          _mm512_storeu_si512((__m512i *)&tptr[tlen], q0);
+          tlen += 16;
           sptr += 16;
           slen -= 16;
           continue;
@@ -134,17 +137,17 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
           __m128i b1 = _mm256_extracti128_si256(data, 1);
           __m512i q0 = _mm512_cvtepu8_epi32(b0);
           __m512i q1 = _mm512_cvtepu8_epi32(b1);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size], q0);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size + 16], q1);
-          t->size += 32;
+          _mm512_storeu_si512((__m512i *)&tptr[tlen], q0);
+          _mm512_storeu_si512((__m512i *)&tptr[tlen + 16], q1);
+          tlen += 32;
           sptr += 32;
           slen -= 32;
           continue;
         } else if ((mask & 0xFFFF) == 0) {
           __m128i b0 = _mm256_extracti128_si256(data, 0);
           __m512i q0 = _mm512_cvtepu8_epi32(b0);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size], q0);
-          t->size += 16;
+          _mm512_storeu_si512((__m512i *)&tptr[tlen], q0);
+          tlen += 16;
           sptr += 16;
           slen -= 16;
           continue;
@@ -162,8 +165,8 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
         __mmask16 mask = _mm_test_epi8_mask(data, _mm_set1_epi8(0x80));
         if (mask == 0) {
           __m512i q0 = _mm512_cvtepu8_epi32(data);
-          _mm512_storeu_si512((__m512i *)&t->data[t->size], q0);
-          t->size += 16;
+          _mm512_storeu_si512((__m512i *)&tptr[tlen], q0);
+          tlen += 16;
           sptr += 16;
           slen -= 16;
           continue;
@@ -199,9 +202,9 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
       } else if (slen >= 2) {
         unsigned char c1 = sptr[1];
         if (c1 <= 0x7F) {
-          t->data[t->size] = c0;
-          t->data[t->size + 1] = c1;
-          t->size += 2;
+          tptr[tlen] = c0;
+          tptr[tlen + 1] = c1;
+          tlen += 2;
           sptr += 2;
           slen -= 2;
           continue;
@@ -212,31 +215,31 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
         goto utf8_1_byte;
       }
     utf8_1x8:
-      t->data[t->size] = c0;
-      t->data[t->size + 1] = sptr[1];
-      t->data[t->size + 2] = sptr[2];
-      t->data[t->size + 3] = sptr[3];
-      t->data[t->size + 4] = sptr[4];
-      t->data[t->size + 5] = sptr[5];
-      t->data[t->size + 6] = sptr[6];
-      t->data[t->size + 7] = sptr[7];
-      t->size += 8;
+      tptr[tlen] = c0;
+      tptr[tlen + 1] = sptr[1];
+      tptr[tlen + 2] = sptr[2];
+      tptr[tlen + 3] = sptr[3];
+      tptr[tlen + 4] = sptr[4];
+      tptr[tlen + 5] = sptr[5];
+      tptr[tlen + 6] = sptr[6];
+      tptr[tlen + 7] = sptr[7];
+      tlen += 8;
       sptr += 8;
       slen -= 8;
       continue;
     utf8_1x4:
-      t->data[t->size] = c0;
-      t->data[t->size + 1] = sptr[1];
-      t->data[t->size + 2] = sptr[2];
-      t->data[t->size + 3] = sptr[3];
-      t->size += 4;
+      tptr[tlen] = c0;
+      tptr[tlen + 1] = sptr[1];
+      tptr[tlen + 2] = sptr[2];
+      tptr[tlen + 3] = sptr[3];
+      tlen += 4;
       sptr += 4;
       slen -= 4;
       continue;
     utf8_1x2:
-      t->data[t->size] = c0;
-      t->data[t->size + 1] = sptr[1];
-      t->size += 2;
+      tptr[tlen] = c0;
+      tptr[tlen + 1] = sptr[1];
+      tlen += 2;
       sptr += 2;
       slen -= 2;
       continue;
@@ -254,9 +257,9 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
           c2 = sptr[2];
           c3 = sptr[3];
           if (c2 >= 0xC2 && c2 <= 0xDF && c3 >= 0x80 && c3 <= 0xBF) {
-            t->data[t->size] = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
-            t->data[t->size + 1] = ((c2 & 0x1F) << 6) | (c3 & 0x3F);
-            t->size += 2;
+            tptr[tlen] = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
+            tptr[tlen + 1] = ((c2 & 0x1F) << 6) | (c3 & 0x3F);
+            tlen += 2;
             sptr += 4;
             slen -= 4;
             continue;
@@ -325,31 +328,32 @@ tonyfettes_encoding_v2_decode_utf8_to_char_array(
     }
     goto malformed;
   utf8_1_byte:
-    t->data[t->size] = c0;
-    t->size++;
+    tptr[tlen] = c0;
+    tlen++;
     sptr += 1;
     slen -= 1;
     continue;
   utf8_2_bytes:
-    t->data[t->size] = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
-    t->size++;
+    tptr[tlen] = ((c0 & 0x1F) << 6) | (c1 & 0x3F);
+    tlen++;
     sptr += 2;
     slen -= 2;
     continue;
   utf8_3_bytes:
-    t->data[t->size] = ((c0 & 0x0F) << 12) | ((c1 & 0x3F) << 6) | (c2 & 0x3F);
-    t->size++;
+    tptr[tlen] = ((c0 & 0x0F) << 12) | ((c1 & 0x3F) << 6) | (c2 & 0x3F);
+    tlen++;
     sptr += 3;
     slen -= 3;
     continue;
   utf8_4_bytes:
-    t->data[t->size] = ((c0 & 0x07) << 18) | ((c1 & 0x3F) << 12) |
-                       ((c2 & 0x3F) << 6) | (c3 & 0x3F);
-    t->size++;
+    tptr[tlen] = ((c0 & 0x07) << 18) | ((c1 & 0x3F) << 12) |
+                 ((c2 & 0x3F) << 6) | (c3 & 0x3F);
+    tlen++;
     sptr += 4;
     slen -= 4;
     continue;
   malformed:
+    t->size += tlen;
     return sptr - s.bytes + s.offset;
   }
 }
